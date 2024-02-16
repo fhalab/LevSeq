7d40c25e1949e978fa111ed88fa2c663fc874c20
YuemingLong <ylong@caltech.edu>
2024-02-15 22:36:35 -0800

    Add importlib library to locate path to executable

--- minION/run_MinION.py
+++ minION/run_MinION.py
@@ -7,6 +7,7 @@ from minION.variantcaller import *
 from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
+from importlib import resources
 import subprocess
 import sys
 import importlib
@@ -50,7 +51,7 @@ def basecall_reads(cl_args):
 # Filter barcode
 def filter_bc(cl_args, result_folder):
     front_min,front_max,back_min,back_max = barcode_user(cl_args)
-    barcode_path_std = "minION/barcoding/minion_barcodes.fasta" # Path to standard barcode file
+    barcode_path = "minION/barcoding/minion_barcodes.fasta" # Path to standard barcode file
     front_prefix = "NB"
     back_prefix = "RB"
     bp = IO_processor.BarcodeProcessor(barcode_path, front_prefix, back_prefix)
@@ -68,9 +69,11 @@ def parent_fasta(cl_args):
 
 # Demultiplex the basecalled fastq into plate-well folders
 def demux_fastq(file_to_fastq, result_folder, barcode_path):
-    path_to_code = "source/source/demultiplex"
-    prompt = f"{path_to_code} -f {file_to_fastq} -d {result_folder} -b {barcode_path} -w {100} -r {100}"
-    subprocess.run(prompt, shell=True)
+    # Obtain path of executable from package
+    with resources.path('MinION.source.source', 'demultiplex') as executable_path:
+        # Use subprocess to run the executable
+        prompt = f"{str(executable_path)} -f {file_to_fastq} -d {result_folder} -b {barcode_path} -w {100} -r {100}"
+        subprocess.run(prompt, shell=True)
 
 # Run MinION
 def run_MinION(cl_args, tqdm_fn = tqdm.tqdm):
@@ -93,3 +96,5 @@ def run_MinION(cl_args, tqdm_fn = tqdm.tqdm):
     # Demultiplex if not skipped
     if not cl_args['skip_demultiplexing']:
         demux_fastq(file_to_fastq, result_folder, barcode_path)
+
+    # Call Variants if not skipped
