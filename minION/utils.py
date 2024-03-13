28b8d58f4ded0637d2e2f1f74ce885d379fb73d1
Yueming <JLoong8@MacBook-Pro-90.local>
2024-03-13 10:15:58 -0700

    Change visualization code

--- minION/utils.py
+++ minION/utils.py
@@ -32,8 +32,8 @@ def translate(seq):
         'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
         'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
         'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
-        'TAC': 'Y', 'TAT': 'Y', 'TAA': '-', 'TAG': '-',
-        'TGC': 'C', 'TGT': 'C', 'TGA': '-', 'TGG': 'W',
+        'TAC': 'Y', 'TAT': 'Y', 'TAA': '*', 'TAG': '*',
+        'TGC': 'C', 'TGT': 'C', 'TGA': '*', 'TGG': 'W',
     }
     protein = ""
     if len(seq) % 3 == 0:
