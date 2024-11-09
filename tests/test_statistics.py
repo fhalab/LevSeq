4a9fcf240eea01758ce0f2a6019a0a0cb6d81a02
Ariane Mora <ariane.n.mora@gmail.com>
2024-11-09 10:07:59 +1000

    Added stats

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -57,3 +57,48 @@ class TestStats(TestClass):
                                      msa_path=f'msa.fa')
         well_df.to_csv('test_brian.csv')
 
+    def test_stats_df(self):
+        # Get them w.r.t to a mutation
+        processed_plate_df = pd.DataFrame()
+        from scipy.stats import mannwhitneyu
+        from tqdm import tqdm
+
+        parent = '#PARENT#'
+        value_column = 'pdt'
+        normalise = True
+
+        parent_values = processed_plate_df[processed_plate_df['Mutations'] == parent][value_column].values
+        parent_mean = np.mean(parent_values)
+        parent_sd = np.std(parent_values)
+
+        # if nomrliase normalize with standard normalisation
+        if normalise:
+            processed_plate_df[f'{value_column} standard norm'] = (processed_plate_df[
+                                                                       value_column].values - parent_mean) / parent_sd
+            value_column = f'{value_column} standard norm'
+
+        parent_values = list(processed_plate_df[processed_plate_df['Mutations'] == parent][value_column].values)
+        parent_mean = np.mean(parent_values)
+        parent_sd = np.std(parent_values)
+        sd_cutoff = 1.5  # The number of standard deviations we want above the parent values
+        # Now for all the other mutations we want to look if they are significant, first we'll look at combinations and then individually
+        grouped_by_mutations = processed_plate_df.groupby('Mutations')
+
+        rows = []
+        for mutation, grp in tqdm(grouped_by_mutations):
+            # Get the values and then do a ranksum test
+            if mutation != parent:
+                vals = list(grp[value_column].values)
+                U1, p = None, None
+                # Now check if there are 3 otherwise we just do > X S.D over - won't be sig anyway.
+                if len(grp) > 2:
+                    # Do stats
+                    U1, p = mannwhitneyu(parent_values, vals, method="exact")
+                mean_vals = np.mean(vals)
+                sig = mean_vals > ((sd_cutoff * parent_sd) + parent_mean)
+                rows.append([mutation, len(grp), mean_vals, mean_vals - parent_mean, sig, U1, p])
+        stats_df = pd.DataFrame(rows, columns=['mutation', 'number of wells with mutation', 'mean',
+                                               'amount greater than parent mean', f'greater than > {sd_cutoff} parent',
+                                               'man whitney U stat', 'p-value'])
+        stats_df
+
